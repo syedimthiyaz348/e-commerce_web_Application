8209d3f592ac9473c89576da7624a549d0c35f9e
Imthiyaz <syedaala348@gmail.com>
2024-06-29 04:38:09 +0000

    ecommerce

---
+++ src/components/CartListView/index.js
@@ -0,0 +1,41 @@
+import CartItem from '../CartItem'
+import CartContext from '../../context/CartContext'
+
+import CartSummary from '../CartSummary'
+
+import './index.css'
+
+const CartListView = () => (
+  <CartContext.Consumer>
+    {value => {
+      const {cartList, removeAllCartItems} = value
+
+      const onClickingRemoveButton = () => {
+        removeAllCartItems()
+      }
+
+      return (
+        <div className="cart-list-button-container">
+          <button
+            onClick={onClickingRemoveButton}
+            className="remove-all-button"
+            data-testid="remove"
+            type="button"
+          >
+            Remove All
+          </button>
+          <ul className="cart-list">
+            {cartList.map(eachCartItem => (
+              <CartItem key={eachCartItem.id} cartItemDetails={eachCartItem} />
+            ))}
+          </ul>
+          <div className="cart-summary-view">
+            <CartSummary />
+          </div>
+        </div>
+      )
+    }}
+  </CartContext.Consumer>
+)
+
+export default CartListView
